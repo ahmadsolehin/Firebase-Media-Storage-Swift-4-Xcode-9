6da12724ea18046c87c6348bca710835c5344e3c
ahmad solehin <solehinjr@gmail.com>
2018-01-09 18:23:48 +0800

    haha

---
+++ Storage On Firebase/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Storage On Firebase
+//
+//  Created by ahmad solehin on 7/23/17.
+//  Copyright © 2017 solehin. All rights reserved.
+//
+
+import UIKit
+import FirebaseStorage
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var uploadImage: UIImageView!
+    @IBOutlet weak var downloadImage: UIImageView!
+    
+    let filename = "earth.jpg"
+    
+    var imageReference: StorageReference {
+        return Storage.storage().reference().child("images")
+    }
+    
+    @IBAction func onUploadTapped() {
+        guard let image = uploadImage.image else { return }
+        guard let imageData = UIImageJPEGRepresentation(image, 1) else { return }
+        
+        let uploadImageRef = imageReference.child(filename)
+        
+        let uploadTask = uploadImageRef.putData(imageData, metadata: nil) { (metadata, error) in
+            print("UPLOAD TASK FINISHED")
+            print(metadata ?? "NO METADATA")
+            print(error ?? "NO ERROR")
+        }
+        
+        uploadTask.observe(.progress) { (snapshot) in
+            print(snapshot.progress ?? "NO MORE PROGRESS")
+        }
+        
+        uploadTask.resume()
+        
+    }
+    
+    @IBAction func onDownloadTapped() {
+        let downloadImageRef = imageReference.child(filename)
+        
+        let downloadtask = downloadImageRef.getData(maxSize: 1024 * 1024 * 12) { (data, error) in
+            if let data = data {
+                let image = UIImage(data: data)
+                self.downloadImage.image = image
+            }
+            print(error ?? "NO ERROR")
+        }
+        
+        downloadtask.observe(.progress) { (snapshot) in
+            print(snapshot.progress ?? "NO MORE PROGRESS")
+        }
+        
+        downloadtask.resume()
+        
+    }
+}
+
